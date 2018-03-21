b0023bf501b3464c7e43a4fb04e9857e79262b1a
Dustin Small <dustins2020@gmail.com>
2018-03-20 19:52:33 -0700

    Example of parsing json

--- app.js
+++ app.js
@@ -13,8 +13,30 @@ var logger = function(req, res, next){
 app.use(logger);
 */
 
+// Body Parser Middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+// Set Static Path
+app.use(express.static(path.join(__dirname, 'public')));
+
+var people = [
+  {
+    name: 'Jeff',
+    age: 30
+  },
+  {
+    name: 'Sarah',
+    age: 52
+  },
+  {
+    name: 'Melinda',
+    age: 20
+  }
+]
+
 app.get('/', function(req, res){
-  res.send('Hello World!');
+  res.json(people);
 });
 
 app.listen(3000, function(){
