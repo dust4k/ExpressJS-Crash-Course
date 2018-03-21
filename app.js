92e22b859cb60cc02fb2fd6eba652501ef9120bb
Dustin Small <dustins2020@gmail.com>
2018-03-20 21:40:13 -0700

    added form validation eith express-validator

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
+const expressValidator = require('express-validator');
 
 const app = express();
 
@@ -17,13 +18,21 @@ app.use(logger);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
-
 // Body Parser Middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
 // Set Static Path
 app.use(express.static(path.join(__dirname, 'public')));
+
+// Global Vars
+app.use(function(req, res, next){
+  res.locals.errors = null;
+  next();
+});
+
+// Express validator middleware
+app.use(expressValidator());
 
 /* Example of parsing json
 var people = [
@@ -68,12 +77,35 @@ var users = [
 ]
 
 app.get('/', function(req, res){
-  var title = 'Customers';
   res.render('index', {
     title: 'Customers',
     users: users
   });
 });
+
+app.post('/users/add', function(req, res){
+  req.checkBody('first_name', 'First Name is required').notEmpty();
+  req.checkBody('last_name', 'Last Name is required').notEmpty();
+  req.checkBody('email', 'Email is required').notEmpty();
+
+  var errors = req.validationErrors();
+
+  if(errors) {
+    res.render('index', {
+      title: 'Customers',
+      users: users,
+      errors: errors
+    });
+  } else {
+    var newUser = {
+      first_name: req.body.first_name,
+      last_name: req.body.last_name,
+      email: req.body.email
+    }
+
+    console.log('SUCCESS');
+  }
+})
 
 app.listen(3000, function(){
   console.log('Server started on Port 3000...');
